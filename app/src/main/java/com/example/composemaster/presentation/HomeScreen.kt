933a46c02501fba214791f9089e511000cf81496
omprasadrathod <20rathodomprasad@gmail.com>
2026-07-16 00:13:33 +0530

    Initialize project with Jetpack Compose and Material 3

---
+++ app/src/main/java/com/example/composemaster/presentation/HomeScreen.kt
@@ -0,0 +1,33 @@
+package com.example.composemaster.presentation
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun HomeScreen(
+    modifier: Modifier = Modifier
+) {
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Welcome to Jetpack Compose")
+        Button(
+            onClick = {}
+        ) {
+            Text("Start Learning")
+        }
+    }
+}
